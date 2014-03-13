f371286557e282e837fb9204bc7fef0dc27bd5f5
Ariekor <851s2001@gmail.com>
2014-03-13 15:59:37 -0400

    Sa marche pas :'(

--- TP2 - Stages/GestionStage.cs
+++ TP2 - Stages/GestionStage.cs
@@ -16,6 +16,7 @@ namespace TP2___Stages
         public OracleConnection conn = new OracleConnection();
         private DataSet mainDataSet = new DataSet();
         int NumStageDefaut = 0;
+        string PackageStage = "GESTIONSTAGES";
         public GestionStage()
         {
             InitializeComponent();
@@ -24,6 +25,7 @@ namespace TP2___Stages
         private void GestionStage_Load(object sender, EventArgs e)
         {
             this.BackColor = Properties.Settings.Default.CouleurFond;
+            MiseAJour();
         }
 
         private void BTN_Ok_Click(object sender, EventArgs e)
@@ -35,6 +37,37 @@ namespace TP2___Stages
         private void BTN_Cancel_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+        private void MiseAJour()
+        {
+            RemplirDGVStage();
+        }
+        private void RemplirDGVStage()
+        {
+            OracleCommand oraliste = new OracleCommand(PackageStage, conn);
+            oraliste.CommandText = PackageStage + ".LISTER";
+            oraliste.CommandType = CommandType.StoredProcedure;
+
+
+            OracleParameter OrapameResultat = new OracleParameter("LISTE",
+           OracleDbType.RefCursor);
+            OrapameResultat.Direction = ParameterDirection.ReturnValue;
+            oraliste.Parameters.Add(OrapameResultat);
+
+            
+
+            OracleDataAdapter Oraliste = new OracleDataAdapter(oraliste);
+
+            if (mainDataSet.Tables.Contains("Liste_stage"))
+            {
+                mainDataSet.Tables["Liste_stage"].Clear();
+            }
+            Oraliste.Fill(mainDataSet, "Liste_stage");
+            Oraliste.Dispose();
+
+            BindingSource maSource = new BindingSource(mainDataSet, "Liste_stage");
+
+            DGV_GestionStage.DataSource = maSource;
         }
     }
 }
