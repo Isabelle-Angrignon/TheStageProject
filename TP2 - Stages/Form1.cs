bcf7400f5448559b4955bb8c92ea68511b1ae453
Waverealm <waverealm@gmail.com>
2014-03-14 15:01:14 -0400

    Form principale - Ya des trucs dein DGV sti

--- TP2 - Stages/Form1.cs
+++ TP2 - Stages/Form1.cs
@@ -15,6 +15,8 @@ namespace TP2___Stages
     {
         private OracleConnection conn = new OracleConnection();
         private DataSet mainDataSet = new DataSet();
+        private string Package = "GESTIONAFFECTATION";
+
         public FormPrincipale()
         {
             InitializeComponent();
@@ -27,6 +29,7 @@ namespace TP2___Stages
             if (GestionS.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 MettreAJour();
+                UpdateDGV();
             }
         }
 
@@ -45,6 +48,7 @@ namespace TP2___Stages
             }
             BN_TypeInfo.Text = Nom;
             MettreAJour();
+            UpdateDGV();
         }
 
         private void FormPrincipale_Load(object sender, EventArgs e)
@@ -52,8 +56,10 @@ namespace TP2___Stages
             Connect();
             Properties.Settings.Default.CouleurFond = Color.LightBlue;
             MettreAJour();
+            UpdateDGV();
             TTIP_profil.SetToolTip(BN_TypeInfo, "Changer de profil");
         }
+
         private void MettreAJour()
         {
             this.BackColor = Properties.Settings.Default.CouleurFond;
@@ -85,6 +91,98 @@ namespace TP2___Stages
         private void BN_TypeInfo_MouseHover(object sender, EventArgs e)
         {
             TTIP_profil.Active = true;
+        }
+
+        private void UpdateDGV()
+        {
+            RemplirDGVEtudiants();
+            RemplirDGVStages();
+            RemplirDGVPlaces();
+        }
+
+        private void RemplirDGVEtudiants()
+        {
+            OracleCommand oraliste = new OracleCommand(Package, conn);
+            oraliste.CommandText = Package + ".LISTESTAGIAIRES";
+            oraliste.CommandType = CommandType.StoredProcedure;
+
+            OracleParameter OrapameResultat = new OracleParameter("ENREG",
+            OracleDbType.RefCursor);
+            OrapameResultat.Direction = ParameterDirection.ReturnValue;
+            oraliste.Parameters.Add(OrapameResultat);
+
+            OracleParameter OrapamDesc = new
+            OracleParameter("PTYPE", OracleDbType.Varchar2);
+            OrapamDesc.Value = BN_TypeInfo.Text;
+            OrapamDesc.Direction = ParameterDirection.Input;
+            oraliste.Parameters.Add(OrapamDesc);
+
+            OracleDataAdapter Oraliste = new OracleDataAdapter(oraliste);
+
+            if (mainDataSet.Tables.Contains("Liste_Etudiants"))
+            {
+                mainDataSet.Tables["Liste_Etudiants"].Clear();
+            }
+            Oraliste.Fill(mainDataSet, "Liste_Etudiants");
+            Oraliste.Dispose();
+
+            BindingSource maSource = new BindingSource(mainDataSet, "Liste_Etudiants");
+            DGV_Etudiants.DataSource = maSource;
+        }
+
+        private void RemplirDGVStages()
+        {
+            OracleCommand oraliste = new OracleCommand(Package, conn);
+            oraliste.CommandText = Package + ".LISTESTAGESDISPO";
+            oraliste.CommandType = CommandType.StoredProcedure;
+
+            OracleParameter OrapameResultat = new OracleParameter("ENREG",
+            OracleDbType.RefCursor);
+            OrapameResultat.Direction = ParameterDirection.ReturnValue;
+            oraliste.Parameters.Add(OrapameResultat);
+
+            OracleDataAdapter Oraliste = new OracleDataAdapter(oraliste);
+
+            if (mainDataSet.Tables.Contains("Liste_Stages"))
+            {
+                mainDataSet.Tables["Liste_Stages"].Clear();
+            }
+
+            Oraliste.Fill(mainDataSet, "Liste_Stages");
+            Oraliste.Dispose();
+
+            BindingSource maSource = new BindingSource(mainDataSet, "Liste_Stages");
+            DGV_Stages.DataSource = maSource;
+        }
+
+        private void RemplirDGVPlaces()
+        {
+            OracleCommand oraliste = new OracleCommand(Package, conn);
+            oraliste.CommandText = Package + ".LISTEPLACES";
+            oraliste.CommandType = CommandType.StoredProcedure;
+
+            OracleParameter OrapameResultat = new OracleParameter("ENREG",
+            OracleDbType.RefCursor);
+            OrapameResultat.Direction = ParameterDirection.ReturnValue;
+            oraliste.Parameters.Add(OrapameResultat);
+
+            OracleParameter OrapamDesc = new
+            OracleParameter("PTYPE", OracleDbType.Varchar2);
+            OrapamDesc.Value = BN_TypeInfo.Text;
+            OrapamDesc.Direction = ParameterDirection.Input;
+            oraliste.Parameters.Add(OrapamDesc);
+
+            OracleDataAdapter Oraliste = new OracleDataAdapter(oraliste);
+
+            if (mainDataSet.Tables.Contains("Liste_Places"))
+            {
+                mainDataSet.Tables["Liste_Places"].Clear();
+            }
+            Oraliste.Fill(mainDataSet, "Liste_Places");
+            Oraliste.Dispose();
+
+            BindingSource maSource = new BindingSource(mainDataSet, "Liste_Places");
+            DGV_Places.DataSource = maSource;
         }
     }
 }
